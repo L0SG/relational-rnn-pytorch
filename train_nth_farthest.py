3db7e86569674b8064eef54c29e1374d83cf4a0a
Jessica Yung <jessicayungyc@gmail.com>
2018-09-13 10:38:09 +0100

    fix(nth-farthest): add min lr

--- train_nth_farthest.py
+++ train_nth_farthest.py
@@ -180,7 +180,7 @@ loss_fn = torch.nn.CrossEntropyLoss()
 
 optimiser = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimiser, 'min', factor=0.5, patience=5)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimiser, 'min', factor=0.5, patience=5, min_lr=8e-5)
 
 # num_batches = int(len(X_train) / batch_size)
 num_test_batches = int(len(X_test) / batch_size)
